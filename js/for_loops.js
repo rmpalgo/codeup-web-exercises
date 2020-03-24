bd75204b09c9ad2689a6d74054ec9175f8578bfc
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-03-24 14:59:47 -0500

    Completed for loops exercise 2.

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,2 +1,9 @@
 "use strict";
 
+function showMultiplicationTable (num) {
+    for (var i = 1; i <= 10; i++) {
+        console.log(num + ' X ' + [i] + ' = ' + (num * [i]));
+    }
+}
+
+showMultiplicationTable(7);
