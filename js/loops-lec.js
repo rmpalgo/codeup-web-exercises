f89ea55013a5651d4637ebf72a22dd080df0ec93
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-03-24 11:18:01 -0500

    Completed FizzBuzz using if/else, maybe better way of doing this.

--- js/loops-lec.js
+++ js/loops-lec.js
@@ -222,7 +222,15 @@ for (var i = 0; i <= 100; i++) {
 //  If divisible by 3 and 5, log "FizzBuzz"
 //  All other cases, just log the number.
 
-
+for (var i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    }
+}
 
 
 
