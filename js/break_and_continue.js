c1c5a359cd811b7625cda88392a8e09d560d0846
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-03-24 21:09:55 -0500

    If I remove the continue, still working, I don't think I'm applying continue correctly.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,12 +1,12 @@
 "use strict";
 var theChoice;
-// do{
-//     var userOddNumChoice = prompt('Choose an odd number between 1 and 50.');
-//     theChoice = userOddNumChoice;
-//     console.log(isNaN(userOddNumChoice));
-//     console.log(userOddNumChoice);
-// } while (isNaN(userOddNumChoice) || userOddNumChoice === '0' || userOddNumChoice % 2 === 0)
-
+// // do{
+// //     var userOddNumChoice = prompt('Choose an odd number between 1 and 50.');
+// //     theChoice = userOddNumChoice;
+// //     console.log(isNaN(userOddNumChoice));
+// //     console.log(userOddNumChoice);
+// // } while (isNaN(userOddNumChoice) || userOddNumChoice === '0' || userOddNumChoice % 2 === 0)
+//
 while (true) {
     var promptInput = parseInt(prompt('Choose an odd number between 1 and 50.'));
     var input = promptInput;
@@ -25,8 +25,10 @@ console.log(theChoice);
 for (var i = 0; i < 50; i++) {
     if (theChoice === i) {
         console.log('Skipping number ' + i)
-        continue;
-    } else if (i % 2 !== 0) {
+        // continue;
+    }
+    if (i % 2 !== 0) {
         console.log(i)
     }
 }
+
