45f03cbc10584a112021879f33c0f0a60f322fa9
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-03-31 10:51:55 -0500

    Changed radius to 5, completed exercise.

--- js/circle.js
+++ js/circle.js
@@ -31,6 +31,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
+    circle.radius = 5;
 
     // log info about the circle
     console.log("Raw circle information");
