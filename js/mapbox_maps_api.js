e14325cf63664651c4a9ce583360f2049935d1d0
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-03 11:08:32 -0500

    Refactored code to separate zoom levels from input and submit, used blur for event for now

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -20,6 +20,18 @@ var restaurants = [
     }
 ];
 
+
+
+var zoomLevel = document.getElementById('submit');
+var select = document.getElementById('zoom-level');
+var address = document.getElementById('address');
+console.log(address);
+
+
+// zoomLevel.addEventListener('click', getZoom);
+select.addEventListener('blur', getZoom);
+
+
 var option = {
     maxWidth: '300px',
     color: 'red',
@@ -31,20 +43,36 @@ var option = {
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
-    zoom: 6,
+    zoom: 0,
     center: [-97.14, 31.54]
 });
 
+// The zoom functionality worked but there is no state where the center is so hardcoded setCenter
+function getZoom() {
+    map.setZoom(select.value);
+    map.setCenter([-97.14, 31.54]);
+}
 
-
-restaurants.forEach( (restaurant, index) => {
-    geocode(restaurant.address, mapboxToken).then( result => {
-        console.log(result);
-
-        //    add additional code for geocode
-        geoRestaurant(result, restaurant);
-    });
+// Add restaurant
+/*
+restaurants.push({
+    name: 'new restaurant',
+    address: address.value,
+    description: 'Best restaurant in the city of'
 });
+*/
+
+function addRestaurant () {
+    restaurants.forEach( (restaurant, index) => {
+        geocode(restaurant.address, mapboxToken).then( result => {
+            console.log(result);
+
+            //    add additional code for geocode
+            geoRestaurant(result, restaurant);
+        });
+    });
+}
+
 
 function geoRestaurant(result, restaurant) {
 
@@ -52,12 +80,14 @@ function geoRestaurant(result, restaurant) {
         .setHTML(`<h3>${restaurant.name}<br>${restaurant.address}<br><hr> ${restaurant.description} <em>${restaurant.address.split(',')[1]}</em></h3>`)
         .addTo(map);
 
-    new mapboxgl.Marker()
+    new mapboxgl.Marker(option)
         .setLngLat(result)
         .setPopup(popup)
         .addTo(map)
+
 }
 
+addRestaurant();
 
 // geocode('2438 W Anderson Ln ste 100 Austin, TX', mapboxToken).then(result => {
 //     console.log(result);
