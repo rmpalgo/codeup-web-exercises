03b51ac9b703bd5c8be1051241e8f2934164e98d
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-08 15:24:00 -0500

    Starting class selectors

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,12 +1,19 @@
 "use strict";
 
 $(document).ready(function() {
-
+    //ID Selectors
+    /*
     // alert('The DOM has finished loading.');
     console.log('The DOM has finished loading.');
 
     var contents = $('#message').html();
     console.log(contents);
     console.log($('#one').html());
+    // console.log($('#three').html());
+    // console.log($('#three, #three').html());
+    */
+
+    //Class Selectors
+
 
 });
