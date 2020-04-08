2735bae408d7929846c239ae9d6472be5443d660
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-08 15:33:38 -0500

    Highlighted the h1 p and li

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -15,6 +15,10 @@ $(document).ready(function() {
 
     //Class Selectors
 
-    $('.codeup').css('border', '1px solid red');
+    // $('.codeup').css('border', '1px solid red');
 
+    // Element selectors
+
+    $('li').css('font-size', '20px');
+    $('h1, p, li').css('background', 'yellow')
 });
