3669f948c4990b1604f88e8884d653cecffe81f7
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-09 10:10:58 -0500

    Added background color to h1 when clicked

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -19,9 +19,14 @@ $(document).ready(function() {
 
     // Element selectors
 
-    $('li').css('font-size', '20px');
-    //mulitiple selectors
-    $('h1, p, li').css('background', 'yellow');
-    // Get the html or text all of the h1,
-    console.log($('h1').text());
+    // $('li').css('font-size', '20px');
+    // //mulitiple selectors
+    // $('h1, p, li').css('background', 'yellow');
+    // // Get the html or text all of the h1,
+    // console.log($('h1').text());
+
+    $('h1').click( function() {
+        $('h1').css('background-color', 'yellow');
+    });
+
 });
