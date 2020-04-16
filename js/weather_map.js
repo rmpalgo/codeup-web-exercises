f52d94cf4fd220eefc8ed4ebf7f3bf94d2d4eed3
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-16 09:48:04 -0500

    In process of refactoring code so that it can be used for 5 day forecast.

--- js/weather_map.js
+++ js/weather_map.js
@@ -26,12 +26,43 @@
          */
 
         function makeForecast (obj) {
+            let liHTML = ``;
+            /*
+        <ul id="ul-forecast" class="list-group list-group-flush">
+            <li id="weather-date" class="list-group-item text-center">Cras justo odio</li>
+            <li id="weather-temperatures" class="list-group-item text-center"></li>
+            <li id="weather-description" class="list-group-item"></li>
+            <li id="weather-wind" class="list-group-item"></li>
+            <li id="weather-pressure" class="list-group-item"></li>
+         </ul>
+             */
+            // make UL
+            $('#forecast').append(makeUL());
+
+            // make children li's
+            liHTML = `<li id="weather-date" class="list-group-item text-center">${getDate(obj)}</li>
+                      <li id="weather-temperatures" class="list-group-item text-center">
+                      <p>${getTemp(obj)[0]} / ${getTemp(obj)[1]}</p>
+                      <p><img alt="weather-icon" src="http://openweathermap.org/img/w/${getIcon(obj)}.png"></p>
+                      </li>       
+                      <li id="weather-description" class="list-group-item">
+                      <p>Description: ${weatherDescription(obj)[0]}</p>
+                      <p>Humidity: ${weatherDescription(obj)[1]}</p>
+                      </li>
+                        `;
             $('#weather-date').text(getDate(obj));
             $('#weather-temperatures').append(getTemp(obj));
             $('#weather-temperatures').append(getIcon(obj));
             $('#weather-description').append(weatherDescription(obj));
             $('#weather-wind').text(getWind(obj));
             $('#weather-pressure').text(getPressure(obj));
+        }
+
+        /**
+         * @returns {string} string to append ul in HTML
+         */
+        function makeUL () {
+            return `<ul id="ul-forecast" class="list-group list-group-flush"></ul>`
         }
 
         /**
