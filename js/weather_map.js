bcced19e994dbc876e84e2623d76a64d5fcb4dec
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-16 07:29:29 -0500

    Created functions to get description, wind speed, date, and pressure.

--- js/weather_map.js
+++ js/weather_map.js
@@ -14,18 +14,32 @@
             console.log(data);
             let firstData = data.list[0];
             $('#weather-date').text(getDate(firstData));
+            $('#weather-description').append(weatherDescription(firstData));
+            $('#weather-wind').text(getWind(firstData));
+            $('#weather-pressure').text(getPressure(firstData));
         }).fail(function (error) {
             console.error(error);
         });
 
-        function getDate (arr) {
-            let date = arr.dt_txt.split(" ")[0].split("-");
+        function getDate (obj) {
+            let date = obj.dt_txt.split(" ")[0].split("-");
             return `${date[1]} / ${date[2]} / ${date[0]}`
         }
 
-        function weatherDescription (arr) {
+        function weatherDescription (obj) {
             let info = [];
+            info.push(obj.weather[0].description);
+            info.push(obj.main.humidity);
+            return `<p>Description: ${info[0]}</p>
+                    <p>Humidity: ${info[1]}</p>`;
+        }
 
+        function getWind (obj) {
+           return `Wind: ${obj.wind.speed}`;
+        }
+
+        function getPressure (obj) {
+            return `Pressure: ${obj.main.pressure}`;
         }
 
 
