2da2e7a6ba6401e531e67bea99e84f5fa57bf45a
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-22 09:53:40 -0500

    Refactored code for es6 exercise.

--- js/es6.js
+++ js/es6.js
@@ -1,6 +1,4 @@
-/*
- * Complete the TODO items below
- */
+
 const users = [
   {
     name: 'zach',
@@ -48,31 +46,21 @@ users.forEach( user => names.push(user.name) );
 console.log(emails);
 console.log(names);
 
-// TODO: replace `var` with `let` in the following declaration
 let developers = [];
 users.forEach( user => {
-  // TODO: rewrite the code below to use object destructuring assignment
-  //       note that you can also use destructuring assignment in the function
-  //       parameter definition
-  // const name = user.name;
-  // const email = user.email;
-  // const languages = user.languages;
+
   const {name, email, languages} = user;
-  // TODO: rewrite the assignment below to use template strings
   developers.push( `${name}'s email is ${email}, ${name} knows ${languages.join(', ')}` );
 });
 console.log(developers);
 
-// TODO: Use `let` for the following variable
 
 let list = '<ul>';
 
-// TODO: rewrite the following loop to use a for..of loop
 for (let eachPerson of developers) {
 
-  // TODO: rewrite the assignment below to use template strings
   list += '<li>' + eachPerson + '</li>';
-};
+}
 list += '</ul>';
 
 document.getElementById('list').innerHTML = list;
