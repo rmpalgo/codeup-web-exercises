45ed372cc0a742727e8c01e83652c4c58686c3fa
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-22 14:10:05 -0500

    Added input search and event listener for search username.

--- js/promises.js
+++ js/promises.js
@@ -7,7 +7,19 @@ let url = `https://api.github.com/users/${owner}/events`;
 
 const lastCommitPromise = fetch(url, {headers: {'Authorization': `token ${githubToken}`}});
 
-lastCommitPromise.then( response => response.json().then( data => console.log(data[0]['created_at'])) );
+lastCommitPromise.then( response => response.json()
+    .then( data => {
+            let lastCommit = data[0]['created_at'];
+            let renderDate = document.getElementById('lastCommit');
+            let username = document.getElementById('username');
+            let submit = document.getElementById('submit');
+
+            submit.addEventListener('click', (e) => {
+                e.preventDefault();
+               console.log(username.value);
+            });
+
+    }) );
 
 lastCommitPromise.catch( error => console.error(error));
 
