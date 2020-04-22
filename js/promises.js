15b12d683f1acf3a84daeb58624dbf70dfea30d2
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-22 13:58:37 -0500

    Feat: accessed array at index 0 for most current push event.

--- js/promises.js
+++ js/promises.js
@@ -3,11 +3,11 @@
 let githubToken = GITHUB_TOKEN;
 let owner = 'rmpalgo';
 let repo = 'codeup-web-exercises';
-let url = `https://api.github.com/repos/${owner}/${repo}/commits/master`;
+let url = `https://api.github.com/users/${owner}/events`;
 
 const lastCommitPromise = fetch(url, {headers: {'Authorization': `token ${githubToken}`}});
 
-lastCommitPromise.then( response => response.json().then( data => console.log(data)) );
+lastCommitPromise.then( response => response.json().then( data => console.log(data[0]['created_at'])) );
 
 lastCommitPromise.catch( error => console.error(error));
 
